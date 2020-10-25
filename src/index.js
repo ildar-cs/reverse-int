2d69b968db475fef8301d1a2b3a07b738f2e8819
ildar_test <attacker.cs@gmail.com>
2020-10-25 23:18:37 +0500

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n.toString().startsWith("-"))
+        newNumber = n.toString().split("").slice(1).reverse().join("");
+    else newNumber = n.toString().split("").reverse().join("");
+
+    return parseInt(newNumber);
+};
